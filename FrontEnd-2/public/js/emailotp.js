9494c124a855d4da5823b610bc021f59189fa24f
Nawaf207 <218110207@psu.edu.sa>
2024-02-12 19:57:52 +0300

    OTP in js (part 1)

--- FrontEnd-2/public/js/emailotp.js
+++ FrontEnd-2/public/js/emailotp.js
@@ -1,50 +1,24 @@
+emailjs.init("_kDz0wMpArGN7ksvL");
+
 function sendOTP() {
-	const email = document.getElementById('email');
-	const otpverify = document.getElementsByClassName('otpverify')[0];
-
-	let otp_val = Math.floor(Math.random() * 10000);
-
-	let emailbody = `<h2>Your OTP is </h2>${otp_val}`;
-	Email.send({
-    SecureToken : "9ed499ba-d073-4618-9bbe-ebd6fa693cfb",
-    To : email.value,
-    From : "218110207@psu.edu.sa",
-    Subject : "Email OTP using JavaScript",
-    Body : emailbody,
-}).then(
-
-	message => {
-		if (message === "OK") {
-			alert("OTP sent to your email " + email.value);
-
-			otpverify.style.display = "flex";
-			const otp_inp = document.getElementById('otp_inp');
-			const otp_btn = document.getElementById('otp-btn');
-
-			otp_btn.addEventListener('click', () => {
-				if (otp_inp.value == otp_val) {
-					alert("Email address verified...");
-				}
-				else {
-					alert("Invalid OTP");
-				}
-			})
-		}
+	var params = {
+		email: document.getElementById("email").value,
 	}
-);
+	let otp_val = Math.floor(Math.random() * 10000);
+	
+	const serviceID = "service_cucc4pm";
+	const templateID = "Ytemplate_j34g9xp";
+  
+	emailjs
+		.send(serviceID, templateID, params)
+		.then(res=>{
+			document.getElementById("otp_val").value = "";
+		
+			console.log(res);
+			alert("Your message sent successfully!!");
+		})
+		.catch(err=>console.log(err));
+  
 }
-    function validateAndRedirect() {
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('floatingPassword').value;
-        const otp = document.getElementById('otp_inp').value;
-
-        // You should implement your own validation logic for email, password, and OTP
-        // For demonstration purposes, I'm assuming they are correct if they are not empty
-        if (email.trim() !== '' && password.trim() !== '' && otp.trim() !== '') {
-            // Redirect to /home/v1/
-            window.location.href = '/home/v1/';
-        } else {
-            alert('Please fill in all fields correctly.');
-        }
-    }
-
+  
+  
