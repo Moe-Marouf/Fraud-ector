cdbb0e63b6a0752e8961ae3e8c4eaa9cbbb6a088
hm-kurdi <156840813+hm-kurdi@users.noreply.github.com>
2024-02-17 12:03:15 +0300

    Test Case U1-4
    For this commit I fixed the issue with TC U1-4

--- FrontEnd-2/public/js/datepicker.js
+++ FrontEnd-2/public/js/datepicker.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const datepicker = new Pikaday({
-      field: document.getElementById("datepicker"),
-      format: "MM/DD/YYYY", // Customize the date format if needed
-      showYearDropdown: true, // Enable year dropdown
-      yearRange: [1900, moment().year()], // Customize the year range as needed
+  const datepicker = new Pikaday({
+    field: document.getElementById("datepicker"),
+    format: "MM/DD/YYYY", // Used to customize date format
+    showYearDropdown: true, // Enabls year dropdown
+    yearRange: [1900, moment().year()], // Used to customize year range
 
-      //Changed the format to make it work with transactionHistory2
-      onSelect: function (date) {
-        // Format the date to YYYY-MM-DD
-        const formattedDate = moment(date).format("YYYY-MM-DD");
-        window.location.href = window.location.origin + "/transactionhistory2/v1/?date=" + encodeURIComponent(formattedDate);
-      },
-    });
+    //Changed the format to make it work with transactionHistory2
+    onSelect: function (date) {
+      // Format the date to YYYY-MM-DD
+      const formattedDate = moment(date).format("YYYY-MM-DD");
+      // Following code is related to the url that will be present once the date is chosen
+      window.location.href =
+        window.location.origin +
+        "/transactionhistory2/v1/?date=" +
+        encodeURIComponent(formattedDate);
+    },
   });
+});
 
-  
-  
-  
-  //This is for the 2nd history page, to search by date
-  document.getElementById('transaction-date').addEventListener('change', function() {
+//This is for the 2nd history page, to search by date
+document
+  .getElementById("transaction-date")
+  .addEventListener("change", function () {
     var selectedDate = this.value;
     //use selectedDate to fetch and display the transactions for this date
   });
-  
-  
