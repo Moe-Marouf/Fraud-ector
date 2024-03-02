d84e1d2e5218cc7bb253497f433aafe17366554e
Nawaf207 <218110207@psu.edu.sa>
2024-03-02 18:25:20 +0300

    fixing transactionhistory2

--- FrontEnd-2/public/js/chart.js
+++ FrontEnd-2/public/js/chart.js
@@ -33,6 +33,30 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+    //for transactionhistory2
+    function populateTable(data) {
+        var tableBody = document.querySelector("#transaction-table2 tbody");
+        tableBody.innerHTML = ""; // Clear existing rows
+    
+        // Parse CSV data
+        Papa.parse(data, {
+            header: true,
+            complete: function(results) {
+                results.data.forEach(function(row, index) {
+                    // Create a new row in the table
+                    var newRow = document.createElement("tr");
+                    newRow.innerHTML = `
+                        <td>${index + 1}</td>
+                        <td>${row['nameOrig']}</td>
+                        <td>${row['nameDest']}</td>
+                        <td>${row['amount']}</td>
+                        <td>${row['isFraud']}</td>
+                    `;
+                    tableBody.appendChild(newRow);
+                });
+            }
+        });
+    }   
  // Function to create pie chart based on type data
  function createPieChartType(data) {
     var ctxPieType = document.getElementById("pie-chart-type").getContext("2d");
