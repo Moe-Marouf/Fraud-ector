0c24cb5fd57f51b2aaa90cccf36f10c32cc8a437
hm-kurdi <156840813+hm-kurdi@users.noreply.github.com>
2024-04-25 17:18:00 +0300

    Changes to app.js
    Some changes to the app.js trying to debug the issue.

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -103,19 +103,32 @@ app.use((req, res) => {
 });
 
 // Connecting Mongoose
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.log("Error connecting to MongoDB:", error);
+//   });
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
+    
+    // Start the server after MongoDB connection is established
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on Port ${process.env.PORT}`);
+    });
   })
   .catch((error) => {
     console.log("Error connecting to MongoDB:", error);
   });
 
 // Port
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on Port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Listening on Port ${process.env.PORT}`);
+// });
 
 //FLASK:
 
