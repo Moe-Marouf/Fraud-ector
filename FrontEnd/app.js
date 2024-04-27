32d0caf614592ad9f6c4100461b802c9d5c2712b
Marouf709 <marouf2002@gmail.com>
2024-04-28 01:04:54 +0300

    Ignore

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -159,7 +159,7 @@ mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
-    
+
     // Start the server after MongoDB connection is established
     app.listen(process.env.PORT, () => {
       console.log(`Listening on Port ${process.env.PORT}`);
@@ -188,12 +188,12 @@ mongoose
 const axios = require('axios');
 async function makePrediction(features) {
   try {
-      const response = await axios.post('http://localhost:5000/predict', {
-          features: features
-      });
-      console.log('Prediction:', response.data);
+    const response = await axios.post('http://localhost:5000/predict', {
+      features: features
+    });
+    console.log('Prediction:', response.data);
   } catch (error) {
-      console.error('Error making prediction:', error);
+    console.error('Error making prediction:', error);
   }
 }
 makePrediction([[-100, -100]]);
